db0e7c1e2f37583ab658d4b9d292b10b70e34d04
M4DD7-b <mbmontgomery06@gmail.com>
2026-04-22 08:40:04 +0100

    updated js

--- Web App/JS/ReportJS/maddyReport.js
+++ Web App/JS/ReportJS/maddyReport.js
@@ -72,11 +72,6 @@ async function loadTurnoutData() {
                         });
                     }
                 }
-            },
-            plugins: {
-                legend: {
-                    position: 'right'
-                }
             }
         });
 
